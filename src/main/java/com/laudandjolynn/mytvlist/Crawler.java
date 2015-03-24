fca7ab9cb0f075bbfbaefd47c8a2e9759c69b6a2
huangtiande <htd0324@gmail.com>
2015-03-24 14:25:30 +0800

    init project.

---
+++ src/main/java/com/laudandjolynn/mytvlist/Crawler.java
@@ -0,0 +1,11 @@
+package com.laudandjolynn.mytvlist;
+
+/**
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2015年3月24日 下午2:25:00
+ * @copyright: www.laudandjolynn.com
+ */
+public class Crawler {
+
+}
