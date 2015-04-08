0dcd51f30bf087dea307bdcf34698ecf9e1f7546
huangtiande <htd0324@gmail.com>
2015-04-08 13:37:08 +0800

    fix bug

--- src/main/java/com/laudandjolynn/mytv/RmiServer.java
+++ src/main/java/com/laudandjolynn/mytv/RmiServer.java
@@ -1,10 +1,16 @@
 package com.laudandjolynn.mytv;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,7 +38,9 @@ public class RmiServer implements Server {
 		logger.info("epg bind to: " + url);
 		try {
 			JolynnTv jolynnTv = new JolynnTvRmiImpl(new JolynnTvImpl());
-			LocateRegistry.createRegistry(Config.NET_CONFIG.getRmiPort());
+			StaticIpSocketFactory factory = new StaticIpSocketFactory();
+			LocateRegistry.createRegistry(Config.NET_CONFIG.getRmiPort(),
+					factory, factory);
 			Naming.bind(url, jolynnTv);
 		} catch (RemoteException e) {
 			throw new MyTvException("error occur while start RMI server.", e);
@@ -44,4 +52,19 @@ public class RmiServer implements Server {
 		logger.info("RMI server started. " + url);
 	}
 
+	private final static class StaticIpSocketFactory implements
+			RMIServerSocketFactory, RMIClientSocketFactory {
+
+		@Override
+		public ServerSocket createServerSocket(int port) throws IOException {
+			return new ServerSocket(Config.NET_CONFIG.getRmiPort(), 0,
+					InetAddress.getByName(Config.NET_CONFIG.getIp()));
+		}
+
+		@Override
+		public Socket createSocket(String host, int port) throws IOException {
+			return new Socket(Config.NET_CONFIG.getIp(),
+					Config.NET_CONFIG.getRmiPort());
+		}
+	}
 }
--- src/main/java/com/laudandjolynn/mytv/utils/Constant.java
+++ src/main/java/com/laudandjolynn/mytv/utils/Constant.java
@@ -59,4 +59,5 @@ public class Constant {
 			file.mkdirs();
 		}
 	}
+
 }
