0cd431caeca802d6f79f2cb3fa64ed8187da571a
huangtiande <htd0324@gmail.com>
2015-05-13 18:25:34 +0800

    add debug bat

--- src/main/assembly/debug.bat
+++ src/main/assembly/debug.bat
@@ -1,1 +1,2 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=tv.1eq1.com,address=8888 -Xms512m -Xmx512m -jar mytv-1.1.jar
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8888 -Xms512m -Xmx512m -jar mytv-1.1.jar
+:: java -Xdebug -Xrunjdwp:transport=dt_socket,address=tv.1eq1.com:8888,suspend=y -Xms512m -Xmx512m -jar mytv-1.1.jar
