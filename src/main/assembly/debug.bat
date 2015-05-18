683f059647d097ce132d2328bd57cd6e8bee837c
Laud <eventer@163.com>
2015-05-18 13:15:26 +0800

    Update debug.bat

--- src/main/assembly/debug.bat
+++ src/main/assembly/debug.bat
@@ -1,2 +1,4 @@
+:: socket_attach
 java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8888 -Xms512m -Xmx512m -jar mytv-1.1.jar
+:: socket_listen
 :: java -Xdebug -Xrunjdwp:transport=dt_socket,address=tv.1eq1.com:8888,suspend=y -Xms512m -Xmx512m -jar mytv-1.1.jar
